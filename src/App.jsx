6d56c98f3e504985bbf32f5f7303c90d6e052c84
PEOPLE Summer Program <bp-ppl-summer@ad.wisc.edu>
2024-07-09 09:36:26 -0500

    did stuff

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,50 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   
   return ( <div>
+
+    <h1>Alex Krostag's Website</h1>
+
+    <hr></hr>
+
+    <p>Hi. The following list will have the first three letters and vegetables that start with that letter.</p>
     
+    <ol>
+      <li>A is for apple</li>
+      <li>B is for banana</li>
+      <li>C is for carrot</li>
+    </ol>
+
+    <hr></hr>
+
+    <p>
+      The vegetables are pictured below.
+    </p>
+
+    <Image
+        src="src/assets/apple.png"
+        rounded
+        width="30%"
+        style={ {paddingTop: 60, paddingBottom: 60} }
+    />
+    
+    <Image
+        src="src/assets/banana.png"
+        rounded
+        width="30%"
+        style={ {paddingTop: 60, paddingBottom: 60} }
+    />
+    
+    <Image
+        src="src/assets/carrots.png"
+        rounded
+        width="30%"
+        style={ {paddingTop: 60, paddingBottom: 60} }
+    />
+
+    {/*
+      make a button
+    */}
+
   </div>
   )
 }
